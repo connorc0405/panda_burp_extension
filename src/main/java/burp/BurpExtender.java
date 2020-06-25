34e2d0e366329ed91b78a474fc5caee62fab1ad9
Connor Cumming <connorc0405@gmail.com>
2020-06-24 22:43:08 -0400

    Connecting to socket in container

--- src/main/java/burp/BurpExtender.java
+++ src/main/java/burp/BurpExtender.java
@@ -1,33 +1,51 @@
 package burp;
 
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
 
 public class BurpExtender implements IBurpExtender
 {
     //
     // implement IBurpExtender
     //
+
+    private final String pandaServerIp = "localhost";
+    private final int pandaServerPort = 8081;
     
     @Override
     public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks)
     {
         // set our extension name
-        callbacks.setExtensionName("Hello world extension");
-        
+        callbacks.setExtensionName("PANDA HTTP CMP Analysis");
+
         // obtain our output and error streams
         PrintWriter stdout = new PrintWriter(callbacks.getStdout(), true);
         PrintWriter stderr = new PrintWriter(callbacks.getStderr(), true);
-        
+
         // write a message to our output stream
         stdout.println("Hello output");
-        
-        // write a message to our error stream
-        stderr.println("Hello errors");
-        
-        // write a message to the Burp alerts tab
-        callbacks.issueAlert("Hello alerts");
-        
-        // throw an exception that will appear in our error stream
-        throw new RuntimeException("Hello exceptions");
+
+        Socket sock = new Socket();
+        InetAddress addr;
+        try {
+            addr = InetAddress.getByName(pandaServerIp);
+        } catch (java.net.UnknownHostException e) {
+            stderr.println("Oopsie addr error :(");
+            return;
+        }
+
+        SocketAddress sockAddr = new InetSocketAddress(addr, pandaServerPort);
+
+        try {
+            sock.connect(sockAddr);
+        } catch (IOException e) {
+            stderr.println("Oopsie socket error :(");
+            return;
+        }
+
     }
 }
