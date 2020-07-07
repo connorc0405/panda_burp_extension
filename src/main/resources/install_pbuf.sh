af5694e513abb0966de76fdfee190aa2861f55de
Connor Cumming <connorc0405@gmail.com>
2020-07-06 23:37:17 -0400

    Rework protobufs

--- src/main/resources/install_pbuf.sh
+++ src/main/resources/install_pbuf.sh
@@ -3,4 +3,4 @@
 # Shamelessly stolen from StackOverflow
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-protoc -I=${dir} --java_out=${dir}/../java/ ${dir}/panda_messages.proto
+protoc -I=${dir} --java_out=${dir}/../java/ ${dir}/panda_messages.proto --experimental_allow_proto3_optional
