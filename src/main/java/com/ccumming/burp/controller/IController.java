048e21db39a267ad16c7989267d5da4ba59bb402
Connor Cumming <connorc0405@gmail.com>
2020-07-07 00:09:48 -0400

    Add send/recv methods to controller interface and controller

--- src/main/java/com/ccumming/burp/controller/IController.java
+++ src/main/java/com/ccumming/burp/controller/IController.java
@@ -1,7 +1,25 @@
 package com.ccumming.burp.controller;
 
+import com.google.protobuf.Message;
+
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.Socket;
 
 public interface IController extends ActionListener {
+
+  /**
+   * Send a {@link Message} over the provided {@link Socket}.
+   * @param message the message to send.
+   * @param sock the socket to use.
+   */
+  public void sendMessage(Message message, Socket sock) throws IOException;
+
+  /**
+   * Return a {@link Message} from the provided {@link Socket}.
+   * @param sock the socket to read from.
+   * @return the received {@link Message}.
+   */
+  public Message recvMessage(Socket sock) throws IOException;
 
 }
