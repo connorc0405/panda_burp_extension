7cf4f9b124f186e422955e2a2cfdeb70a3d0489c
Connor Cumming <connorc0405@gmail.com>
2020-07-06 23:57:24 -0400

    Move old code to scratch space

--- src/main/java/com/ccumming/burp/controller/Controller.java
+++ src/main/java/com/ccumming/burp/controller/Controller.java
@@ -42,45 +42,5 @@ public class Controller implements IController {
   }
 
 
-//        // connect to socket
-//        Socket pySock;
-//        try {
-//            pySock = new Socket(pandaServerIp, pandaServerPort);
-//        } catch (IOException e) {
-//            e.printStackTrace(stderr);
-//            return;
-//        }
-//
-//        stdout.println("Connected to socket");
-//
-//        PandaMessages.BurpMessage msg = PandaMessages.BurpMessage.newBuilder().setCommand(
-//                PandaMessages.Command.newBuilder().setCmd("begin_record").build()
-//        ).build();
-//
-//        try {
-//            sendMessage(msg, pySock);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            return;
-//        }
-//        stdout.println("Sent start msg");
-//
-//        try {
-//            Thread.sleep(10000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//
-//        PandaMessages.BurpMessage msg2 = PandaMessages.BurpMessage.newBuilder().setCommand(
-//                PandaMessages.Command.newBuilder().setCmd("end_record").build()
-//        ).build();
-//
-//        try {
-//            sendMessage(msg2, pySock);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            return;
-//        }
-//        stdout.println("Sent stop msg");
 
 }
